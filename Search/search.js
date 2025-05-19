2f8c66abe421d5a596a1150e0d390735d8a5472b
Thiago_19 Notebook <riffothiago19@gmail.com>
2025-05-19 11:18:39 -0300

    Linkee páginas

--- Search/search.js
+++ Search/search.js
@@ -9,10 +9,20 @@ const imageUp = './Fotos/contraer.png';
 let Vuelta1 = false;
 let Vuelta2 = false
 button1.addEventListener('click', () => {
-    Vuelta1 = !Vuelta1;
-    image1.src = Vuelta1 ? imageUp : imageDown;
+  Vuelta1 = !Vuelta1;
+  image1.src = Vuelta1 ? imageUp : imageDown;
 });
 button2.addEventListener('click', () => {
-    Vuelta2 = !Vuelta2;
-    image2.src = Vuelta2 ? imageUp : imageDown;
+  Vuelta2 = !Vuelta2;
+  image2.src = Vuelta2 ? imageUp : imageDown;
 });
+
+
+const productos = document.getElementsByClassName('section__div-div')
+
+for (let i = 0; i < productos.length; i++) {
+  productos[i].addEventListener('click', () => {
+    window.location.href = "../Descripcion/Descripcion.html";
+  })
+
+}
