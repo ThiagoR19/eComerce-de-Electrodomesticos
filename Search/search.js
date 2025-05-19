0e72235181e34b22035d4e0a1c28af868c5da313
Thiago_19 Notebook <riffothiago19@gmail.com>
2025-05-19 17:08:31 -0300

    Termine el header Responsive

--- Search/search.js
+++ Search/search.js
@@ -1,3 +1,5 @@
+// Nahuel
+
 const button1 = document.getElementById('Cate');
 const image1 = document.getElementById('buttonImg1');
 const button2 = document.getElementById('Produc');
@@ -7,7 +9,8 @@ const imageDown = './Fotos/expandir.png';
 const imageUp = './Fotos/contraer.png';
 
 let Vuelta1 = false;
-let Vuelta2 = false
+let Vuelta2 = false;
+
 button1.addEventListener('click', () => {
   Vuelta1 = !Vuelta1;
   image1.src = Vuelta1 ? imageUp : imageDown;
@@ -20,6 +23,8 @@ button2.addEventListener('click', () => {
 
 const productos = document.getElementsByClassName('section__div-div')
 
+// Thiago
+
 for (let i = 0; i < productos.length; i++) {
   productos[i].addEventListener('click', () => {
     window.location.href = "../Descripcion/Descripcion.html";
@@ -27,35 +32,20 @@ for (let i = 0; i < productos.length; i++) {
 
 }
 
-const carousel = document.querySelector('.carousel');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-
-let scrollAmount = 0;
-
-nextButton.onclick = () => {
-  carousel.scrollBy({ left: 625, behavior: 'smooth' });
-};
-
-prevButton.onclick = () => {
-  carousel.scrollBy({ left: -625, behavior: 'smooth' });
-};
-
+// Negro
 
 function toggleMenu() {
   const mobileMenu = document.querySelector('.mobile-menu');
   mobileMenu.classList.toggle('open');
 }
 
-button1.addEventListener('click', () => {
-  Vuelta1 = !Vuelta1;
-  image1.src = Vuelta1 ? 'Fotos/contraer.png' : 'Fotos/expandir.png';
-});
+const button1Mobile = document.getElementById('Cate-mobile');
+const image1Mobile = document.getElementById('buttonImg1-mobile');
+const button2Mobile = document.getElementById('Produc-mobile');
+const image2Mobile = document.getElementById('buttonImg2-mobile');
 
-button2.addEventListener('click', () => {
-  Vuelta2 = !Vuelta2;
-  image2.src = Vuelta2 ? 'Fotos/contraer.png' : 'Fotos/expandir.png';
-});
+let Vuelta1Mobile = false;
+let Vuelta2Mobile = false;
 
 button1Mobile.addEventListener('click', () => {
   Vuelta1Mobile = !Vuelta1Mobile;
@@ -66,12 +56,4 @@ button2Mobile.addEventListener('click', () => {
   Vuelta2Mobile = !Vuelta2Mobile;
   image2Mobile.src = Vuelta2Mobile ? 'Fotos/contraer.png' : 'Fotos/expandir.png';
 });
-
-const button1Mobile = document.getElementById('Cate-mobile');
-const image1Mobile = document.getElementById('buttonImg1-mobile');
-const button2Mobile = document.getElementById('Produc-mobile');
-const image2Mobile = document.getElementById('buttonImg2-mobile');
-
-let Vuelta1Mobile = false;
-let Vuelta2Mobile = false;
 
